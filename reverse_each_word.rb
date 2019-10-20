9c2ec4bbfebb777bef44cbe46185c29cc518bd95
kelly r maass <kmaass77627@gmail.com>
2019-10-20 04:39:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,4 @@
-def reverse_each_word_v1(sentence)
-  reversed_sentence = ''
-  sentence.split().each do |word|
-    reversed_sentence += word.reverse + ' '
-  end
-  return reversed_sentence.rstrip
-  end 
-  
-  reverse_each_word("Hello there, and how are you?")
+
   
   def reverse_each_word(sentence)
   reversed_sentence = ''
