e3df65a6b53c3246295fe8987c04a6adafbacac4
kelly r maass <kmaass77627@gmail.com>
2019-10-20 04:23:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   reversed_sentence = ''
   sentence.split().each do |word|
-    reversed_sentence += word.reverse + ''
+    reversed_sentence += word.reverse + ' '
   end
   return reversed_sentence.rstrip
   end 
